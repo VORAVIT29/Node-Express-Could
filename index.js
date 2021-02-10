5d8b0810b0b2fa108572c8724c0be0a58d8e8207
VORAVIT29 <6206021610153@fitm.kmutnb.ac.th>
2021-02-10 17:59:00 +0700

    Edit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT
 
 app.get("/", (req, res) => {
-    res.send("Hello Node.js World Voravit Saenmo")
+    res.send("Hello Node.js World")
 })
 
 app.listen(port, () => {
